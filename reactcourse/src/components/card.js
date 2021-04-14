7b50928b8f1f98f6237d2116f96d965220d7f8bc
shawndevp <shawn.ornerstedt99@hotmail.com>
2021-04-14 11:32:19 +0200

    state testing

--- reactcourse/src/components/card.js
+++ reactcourse/src/components/card.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Card(props) {
+    const [counter, setCounter] = useState(0);
+
+    function changeState() {
+
+        setCounter()
+    }
+
     return(
         <>
         <div>
-            <p>Name: {props.name} </p>
+            <button onClick={()=> setCounter(counter + 1)}>+</button>
         </div>
 
+        <button onClick={()=>setCounter(counter - 1)} >-</button>
+
+        <div>{counter}</div>
+
         <div>
-            <p>Price: {props.price}</p>
+            <p>price: {props.price}</p>
             <p>Price: {props.Price}</p>
         </div>
         </>
--- reactcourse/src/components/header.js
+++ reactcourse/src/components/header.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Menu from "./menu.js";
+import Card from "./card.js";
 
 function Header() {
 
     return (
         <>
         <Menu />
-
+        <Card price ="headerpris = 500" Price ="headerpris = 250 stort P" name="test headernamn"/>
         </>
     )
 }
