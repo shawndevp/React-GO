35892123b54b4b3aefddd0b25d072ac7a79ecf28
shawndevp <shawn.ornerstedt99@hotmail.com>
2021-04-16 13:09:41 +0200

    flera inputs cross map

--- reactcourse/src/components/form.js
+++ reactcourse/src/components/form.js
@@ -5,12 +5,18 @@ function Form () {
         // En onSubmit function == handleSubmit
 
         // En onChange function == handleOnChnage
-        const [name, setName] = useState("Mata in ditt namn")
-        const [email, setEmail] = useState("Mata in din email")
+        // const [name, setName] = useState("Mata in ditt namn")
+        // const [email, setEmail] = useState("Mata in din email")
+        const initialValues = {
+            name: "",
+            email: ""
+        }
+
+        const [formValues, setFormValues] = useState(initialValues);
 
     function handleSubmit(e) {
         e.preventDefault();
-        console.log("name_IN:", name, "email_IN:", email)
+        console.log("name_IN:", formValues.name, "email_IN:", formValues.email )
     }
 
     function handleOnChnage(e) {
@@ -19,15 +25,21 @@ function Form () {
         // setEmail(e.target.value)
 
         // console.log({[e.target.name]: e.target.value})
-    }
+    
+
+    setFormValues ( {
+        ...formValues,
+        [e.target.name]:e.target.value
+    })
+}
 
 
     return (
 
         <form onSubmit={handleSubmit}>
 
-            <input placeholder= "Enter your name" name="name" value={name} onChange={handleOnChnage} />    
-            <input placeholder = "Enter your email" name="email" value={email} onChange={handleOnChnage} />
+            <input placeholder= "Enter your name" name="name" value={formValues.name} onChange={handleOnChnage} />    
+            <input placeholder = "Enter your email" name="email" value={formValues.email} onChange={handleOnChnage} />
             <button>Send</button>
 
         </form>
