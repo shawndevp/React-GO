4a094459f34bdd38b4b58bf06454cac704211b11
shawndevp <shawn.ornerstedt99@hotmail.com>
2021-06-02 12:08:09 +0200

    fixing with .env keys

--- tattooshops/src/components/ForgotPassword.js
+++ tattooshops/src/components/ForgotPassword.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
-import fireStore from "./FirebaseConfig"
+import fireStore from "../FirebaseConfig"
 import dotenv from 'dotenv'
 dotenv.config();
 
