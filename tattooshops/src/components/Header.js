0fe87c5bd06c381d739a6b615a2b472bdbade538
shawndevp <shawn.ornerstedt99@hotmail.com>
2021-08-16 18:09:27 +0200

    profile link fixed

--- tattooshops/src/components/Header.js
+++ tattooshops/src/components/Header.js
@@ -44,9 +44,14 @@ function clearLocalStorage() {
     { isLoggedin && jwt?
     <button className="inline-block text-sm px-4 py-2 leading-none border rounded text-black border-black hover:border-transparent hover:text-teal hover:bg-white mt-4 lg:mt-0" onClick={clearLocalStorage}>Logout</button>
     
-    
       : <Link to="/Login" className="inline-block text-sm px-4 py-2 leading-none border rounded text-black border-black hover:border-transparent hover:text-teal hover:bg-white mt-4 lg:mt-0">Login</Link>
       }
+
+      { isLoggedin && jwt? 
+      <Link to="/Profile" className="inline-block text-sm px-4 py-2 leading-none border rounded text-black border-black hover:border-transparent hover:text-teal hover:bg-white mt-4 lg:mt-0">Profile</Link> 
+      : <span></span>
+      }
+      
     </div>
   </div>
 </nav>
