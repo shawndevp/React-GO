dba9e1c6e576805e3a804552a99816358168e317
Shawn Örnerstedt <shawn.ornerstedt99@hotmail.com>
2021-08-20 11:20:14 +0200

    pushes

---
+++ tattooshops/src/components/google-signin.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { GoogleAuthProvider } from "firebase/auth";
+
+function googleSign() {
+
+    const provider = new GoogleAuthProvider();
+
+    
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default googleSign
